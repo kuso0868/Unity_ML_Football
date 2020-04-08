ba0c2d992d45b9e6f6678ddcf80a602c309bda16
kuso0868 <55180664+kuso0868@users.noreply.github.com>
2020-04-08 15:55:06 +0800

    0351

---
+++ Unity_ML_Football/Assets/Scripts/Ball.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public static bool complete;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "進球感應區")
+        {
+            complete = true;
+        }
+    }
+}
